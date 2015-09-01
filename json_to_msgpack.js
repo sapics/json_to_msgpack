9082e8090838e93c45caf29fa738da3a52ee78ed
sapics <gv.nishino@gmail.com>
2015-09-01 18:27:58 +0900

    first commit

---
+++ json_to_msgpack.js
@@ -0,0 +1,45 @@
+var msgpack = require('msgpack-lite'),
+    fs = require('fs')
+
+function eachFiles(filePath, rootPath, callback) {
+    if (!rootPath) {
+        rootPath = filePath;
+    }
+    var stat = fs.statSync(filePath);
+    if (!stat) {
+
+    } else if (stat.isDirectory()) {
+        try {
+            var files = fs.readdirSync(filePath);
+            if (!files) {
+
+            } else {
+                for (var _i in files)(function (i) {
+                    var file = files[i];
+                    if (filePath.match(/.*\/$/)) {
+                        eachFiles(filePath + file, rootPath, callback);
+                    } else {
+                        eachFiles(filePath + "/" + file, rootPath, callback);
+                    }
+                }(_i));
+            }
+        } catch (e1) {
+            console.error("Directory " + filePath + " is unreadable.");
+        }
+    } else if (stat.isFile()) {
+        if (callback) {
+            callback.call(this, filePath, rootPath);
+        }
+    } else {
+        console.error(filePath + " is not file or directory");
+    }
+}
+
+eachFiles(process.argv[2], null, function(filePath){
+    if (!((/\.json/i).test(filePath))) return;
+
+    var rs = fs.createReadStream(filePath),
+        decodeStream = msgpack.createDecodeStream();
+
+    rs.pipe(decodeStream).on('data', console.warn);
+})
